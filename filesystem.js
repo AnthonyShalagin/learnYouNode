6207b6061c3c951aad4ee0b01a9afb7a664a8691
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-12-25 16:55:58 -0500

    IO tutorial

---
+++ filesystem.js
@@ -0,0 +1,15 @@
+/*
+Buffer objects are Node's way of efficiently representing arbitrary arrays  
+  of data, whether it be ascii, binary or some other format. Buffer objects  
+  can be converted to strings by simply calling the toString() method on  
+  them. e.g. var str = buf.toString().  
+*/
+
+var fs = require('fs');
+
+var contents = fs.readFileSync(process.argv[2]);
+
+var str = contents.toString()
+var lines = str.split('\n');
+
+console.log(lines.length-1);
--- program.js
+++ program.js
@@ -8,4 +8,3 @@ for (var i = 2; i < process.argv.length; i++) {
 	sum += Number(process.argv[i]);
 }
 
-console.log(sum);
