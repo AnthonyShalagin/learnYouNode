aa1822b7b5d10862ef0c7120c96ab7e60d97b196
Anthony Shalagin <anthony.shalagin@gmail.com>
2017-12-31 13:19:08 -0500

    another example

--- callbacksArrowFunctionsPromises/arrowFunctions.js
+++ callbacksArrowFunctionsPromises/arrowFunctions.js
@@ -10,4 +10,11 @@ var square2 = (x) => x * x;
 console.log(square1(4));
 console.log(square2(4));
 
+var user = {
+	name: 'Anthony',
+	sayHi: () => {
+		console.log(`Hi ` + user.name);
+	}
+};
 
+user.sayHi();
