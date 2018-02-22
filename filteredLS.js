f9ffad8df2986b8d8b4e6d32cfaf66477b5f393f
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-22 18:35:20 -0500

    modular.js

--- filteredLS.js
+++ filteredLS.js
@@ -45,7 +45,7 @@ fs.readdir(process.argv[2], function callback(err, list) {
   
   for (var i = 0; i < list.length; i++) {
   	if (process.argv[3] == path.extname(list[i]).substring(1)) {
-  		console.log(list[i]);
+  		console.log(list[i]);	
   	}
   }
 
