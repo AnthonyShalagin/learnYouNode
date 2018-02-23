15685ec21d0de3f1c638832230e57bc738951121
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-22 20:07:39 -0500

    modular solution

--- modular.js
+++ modular.js
@@ -1,7 +1,10 @@
-/*
-	Create a program that prints a list of files in a given directory,  
-	filtered by the extension of the files. The first argument is the  
-	directory name and the second argument is the extension filter. Print the  
-	list of files (one file per line) to the console. You must use  
-	asynchronous I/O.  
- */
+var lslib = require('./lslib');
+
+var dirname = process.argv[2];
+var ext = process.argv[3];
+
+lslib(dirname, ext, function(err, files) {
+  for (i = 0; i < files.length; i++) {
+    console.log(files[i]);
+  }
+});
