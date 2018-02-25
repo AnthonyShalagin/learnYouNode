a8cb57db4cf8d4f48bd03c65716634fbcee8b629
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-25 10:40:18 -0500

    timeServer exercise 10

--- timeServerEx10.js
+++ timeServerEx10.js
@@ -17,3 +17,18 @@
   After sending the string, close the connection. 
 */
 
+var date = new Date();
+var net = require('net')  
+
+var server = net.createServer(function (socket) {  
+	
+	var data = date.getFullYear() + "-" + zeroCheck(date.getMonth()+1) + "-" + zeroCheck(date.getDate()) + " " + zeroCheck(date.getHours()) + ":" + zeroCheck(date.getMinutes());
+    socket.write(data);
+    socket.end("\n");
+});
+
+server.listen(process.argv[2]);
+
+function zeroCheck(moment){
+	return moment < 10 ? "0" + moment : moment;
+}
