1c6d042412609a870b21d4cff5a981aee4774770
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-26 15:20:43 -0500

    solution for exercise 11

--- fileServerEx11.js
+++ fileServerEx11.js
@@ -9,3 +9,14 @@
   command-line argument. You must use the fs.createReadStream() method to  
   stream the file contents to the response.  
 */
+
+var http = require('http')
+var fs = require('fs');
+var server = http.createServer(function (req, res) {  
+   var readStream = fs.createReadStream(process.argv[3]);
+   readStream.pipe(res);
+});  
+server.listen(process.argv[2]); 
+
+
+// The pipe() function reads data from a readable stream as it becomes available and writes it to a destination writable stream.
