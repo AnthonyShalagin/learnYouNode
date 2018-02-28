b443195ab6a77adf1e92378bdc53531bca04a9c2
Anthony Shalagin <anthony.shalagin@gmail.com>
2018-02-28 16:46:18 -0500

    exercise 13 solution

--- jsonHTTPAPIServer.js
+++ jsonHTTPAPIServer.js
@@ -24,6 +24,46 @@
      { "unixtime": 1376136615474 }  
    
   Your server should listen on the port provided by the first argument to  
-  your program.  
+  your program. 
+
 */
+
+var http = require('http')
+var url = require('url')
+
+function parsetime (time) {
+  return {
+    hour: time.getHours(),
+    minute: time.getMinutes(),
+    second: time.getSeconds()
+  }
+}
+
+function unixtime (time) {
+  return { unixtime : time.getTime() }
+}
+
+var server = http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url, true);
+  var time = new Date(parsedUrl.query.iso);
+  var result;
+
+  if (/^\/api\/parsetime/.test(req.url)) {
+  	result = parsetime(time); 
+  }
+    
+  else if (/^\/api\/unixtime/.test(req.url)) {
+  	result = unixtime(time);
+  }
+    
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+})
+server.listen(process.argv[2]);
+
 
